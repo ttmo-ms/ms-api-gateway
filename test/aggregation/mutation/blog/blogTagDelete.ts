19b3a122148c14484a4e967c5ef630ff8513cbf0
JoverZhang <joverzh@gmail.com>
2020-07-14 21:21:54 +0800

    test(mutation): add blogTagDelete.test.

---
+++ test/aggregation/mutation/blog/blogTagDelete.ts
@@ -0,0 +1,33 @@
+import { Done } from 'mocha'
+import { aggregationUrl, expect, request, showJson, showTitle } from '../../../index.test'
+
+
+const query = `
+mutation {
+  blogTag (
+    id: "1"
+  ): {
+    id
+    name
+    url
+  }
+}
+`
+
+export default (done: Done) => {
+  showTitle('mutation blogUpdate')
+  request
+    .post(aggregationUrl)
+    .query({ query, validates: null })
+    .expect(200)
+    .end((err, res) => {
+      const { data, code } = res.body
+      expect(code).equal(0)
+      expect(data).to.be.an('object')
+      expect(data.blogTag)
+        .to.be.an('object')
+        .keys(['id', 'name', 'url'])
+      showJson(data)
+      done()
+    })
+}
--- test/aggregation/mutation/index.ts
+++ test/aggregation/mutation/index.ts
@@ -5,6 +5,7 @@ import mutationBlogUpdate from './blog/blogUpdate'
 import mutationBlogDelete from './blog/blogDelete'
 import mutationBlogTagCreate from './blog/blogTagCreate'
 import mutationBlogTagUpdate from './blog/blogTagUpdate'
+import mutationBlogTagDelete from './blog/blogTagDelete'
 
 
 export {
@@ -15,4 +16,5 @@ export {
   mutationBlogDelete,
   mutationBlogTagCreate,
   mutationBlogTagUpdate,
+  mutationBlogTagDelete,
 }
