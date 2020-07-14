cc569926676e69149be4913d700156627fa0ee30
JoverZhang <joverzh@gmail.com>
2020-07-14 13:53:50 +0800

    feat(test/mutation): add blogCreate.

---
+++ test/aggregation/mutation/blog/blogCreate.ts
@@ -0,0 +1,51 @@
+import { Done } from 'mocha'
+import { aggregationUrl, expect, request, showJson, showTitle } from '../../../index.test'
+
+
+const query = `
+mutation {
+  blog: blogCreate (
+    title: "javaScript es6", 
+    typeTags: ["1"],
+    anther: "3",
+    content: "function () { return }"
+  ) {
+    id
+    title
+    typeTags {
+      id
+      name
+      url
+    }
+    anther {
+      id
+      username
+      homepage
+    }
+    readCount
+    favoriteCount
+    createDateTime
+    content
+  }
+}
+`
+
+export default (done: Done) => {
+  showTitle('mutation blogCreate')
+  request
+    .post(aggregationUrl)
+    .query({ query, validates: null })
+    .expect(200)
+    .end((err, res) => {
+      const { data, code } = res.body
+      expect(code).equal(0)
+      expect(data).to.be.an('object')
+      expect(data.blog)
+        .to.be.an('object')
+        .keys(['id', 'title', 'typeTags', 'anther', 'readCount', 'favoriteCount', 'createDateTime', 'content'])
+      expect(data.blog.typeTags).to.be.an('array')
+      expect(data.blog.anther).to.be.an('object').keys('id', 'username', 'homepage')
+      showJson(data)
+      done()
+    })
+}
--- test/aggregation/mutation/index.ts
+++ test/aggregation/mutation/index.ts
@@ -1,8 +1,10 @@
 import mutationUserLogin from './user/login.test'
 import mutationUserRegister from './user/register.test'
+import mutationBlogCreate from './blog/blogCreate'
 
 
 export {
   mutationUserLogin,
   mutationUserRegister,
+  mutationBlogCreate,
 }
